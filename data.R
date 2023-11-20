08ef2a5b3ba5de78294c5f8ffb3312be97f0849d
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2023-11-20 16:43:09 -0500

    Initial commit

---
+++ data.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(googlesheets4)
+gs4_deauth()
+df <- read_sheet("https://docs.google.com/spreadsheets/d/1qxfVQsENxvPyMkrYr_68LoB-pp1I13hDMmJp2lc_UxQ/edit#gid=456228218")
+write_csv(df,file = "AIDSVu_National_Prev_2021_EDITS.csv")
